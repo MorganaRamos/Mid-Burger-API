013c3396dfbe71a084fbbf1cdc56586c6a31cb40
Morgana Ramos <morganaramos@edu.unifor.br>
2022-12-01 00:34:54 -0300

    criando bd fake

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const routes = express.Router()
 
-
+//banco de Dados fake para teste api 
+let bancoFake =[
+    {'1': { Produto: 'Mid Hamburguer', Valor:'30', Categoria:'Hambuguer'}},
+    {'2': { Produto: 'Coca cola', Valor:'9', Categoria:'Bebidas'}},
+    {'3': { Produto: 'Suco Laranja', Valor:'12', Categoria:'Bebidas'}},
+    {'4': { Produto: 'Batata rústica', Valor:'18', Categoria:'Acompanhamento'}}
+]
 
 
 module.exports = routes
